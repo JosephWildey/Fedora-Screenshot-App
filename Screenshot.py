5b821d9ca04feef7d0d278ab2eaee05925223e03
Joseph Wildey <joewildey@outlook.com>
2022-07-04 17:14:47 -0400

    Update Screenshot.py
    Adds a second button to go through the pictures folder and grab the latest picture. It assumes the latest picture is your last screenshot from last session. I could probably fix that assumption a little bit.

--- Screenshot.py
+++ Screenshot.py
@@ -1,5 +1,6 @@
 import tkinter
 import os
+from PIL import Image, ImageTk
 
 window = tkinter.Tk()
 window.title('Take a screenshot')
@@ -10,5 +11,20 @@ def screenshot():
 
 button = tkinter.Button(window, text="Take screenshot!", command = screenshot)
 
+image_dir = "/home/user/Pictures/"
+
+images = os.listdir(image_dir)
+
+last_image = ImageTk.PhotoImage(Image.open(image_dir + images[-1]))
+
+def show_screenshot():
+
+    img_label = tkinter.Label(window, image=last_image)
+    img_label.pack()
+
+show_image_button = tkinter.Button(window, text="Show last screenshot", command = show_screenshot)
+
+
 button.pack()
+show_image_button.pack()
 window.mainloop()
